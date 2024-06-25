bb45ed40d426cdd66655d1d00d21fdb38f725cfa
Ramiro <ramiroglucero03@gmail.com>
2024-06-25 00:21:12 -0300

    final
    inicio de sesion y favorito agregado por los chicos

--- nasa_image_gallery/config/config.py
+++ nasa_image_gallery/config/config.py
@@ -9,3 +9,11 @@ NASA_REST_API = 'https://images-api.nasa.gov/search?q='
 DEFAULT_SEARCH_WORD = 'space'
 
 NASA_REST_API_DEFAULT_SEARCH = NASA_REST_API + DEFAULT_SEARCH_WORD
+
+
+INSTALLED_APPS = [
+
+    'django.contrib.auth',
+    'accounts'
+]
+
--- nasa_image_gallery/layers/services/services_nasa_image_gallery.py
+++ nasa_image_gallery/layers/services/services_nasa_image_gallery.py
@@ -10,11 +10,11 @@ def getAllImages(input=None):
     # ¡OJO! el parámetro 'input' indica si se debe buscar por un valor introducido en el buscador.
     json_collection = transport.getAllImages()
     images = []
+
+    # recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images. Ayuda: ver mapper.py.
     for objeto in json_collection:
         nuevo_nasacard = mapper.fromRequestIntoNASACard(objeto)
         images.append(nuevo_nasacard)
-
-    # recorre el listado de objetos JSON, lo transforma en una NASACard y lo agrega en el listado de images. Ayuda: ver mapper.py.
 
     return images
 
@@ -25,8 +25,8 @@ def getImagesBySearchInputLike(input):
 
 # añadir favoritos (usado desde el template 'home.html')
 def saveFavourite(request):
-    fav = '' # transformamos un request del template en una NASACard.
-    fav.user = '' # le seteamos el usuario correspondiente.
+    fav = mapper.fromTemplateIntoNASACard(request)  # transformamos un request del template en una NASACard.
+    fav.user = get_user(request) # le seteamos el usuario correspondiente.
 
     return repositories.saveFavourite(fav) # lo guardamos en la base.
 
@@ -38,11 +38,11 @@ def getAllFavouritesByUser(request):
     else:
         user = get_user(request)
 
-        favourite_list = [] # buscamos desde el repositorio TODOS los favoritos del usuario (variable 'user').
+        favourite_list = repositories.getAllFavouritesByUser(user) # buscamos desde el repositorio TODOS los favoritos del usuario (variable 'user').
         mapped_favourites = []
 
         for favourite in favourite_list:
-            nasa_card = '' # transformamos cada favorito en una NASACard, y lo almacenamos en nasa_card.
+            nasa_card = mapper.fromRepositoryIntoNASACard(favourite) # transformamos cada favorito en una NASACard, y lo almacenamos en nasa_card.
             mapped_favourites.append(nasa_card)
 
         return mapped_favourites
